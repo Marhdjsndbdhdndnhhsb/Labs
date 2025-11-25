f2b0698854590e4903e76fe3b39aac873bcc9126
Marhdjsndbdhdndnhhsb <msokirko45@gmail.com>
2025-11-25 14:13:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-def pick_max(*args, default=None, abs_mode=False):
-    # Якщо аргументів немає — повертаємо default
-    if not args:
-        return default
-
-    # Якщо abs_mode=True — порівнюємо по абсолютному значенню
-    if abs_mode:
-        return max(args, key=lambda x: abs(x))
-    return max(args)
 
