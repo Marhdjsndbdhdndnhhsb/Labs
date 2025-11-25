b618178c26320054b8d0ce1b81cfd207d0f22e8e
Marhdjsndbdhdndnhhsb <msokirko45@gmail.com>
2025-11-25 14:05:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Labs
+def pick_max(*args, default=None, abs_mode=False):
+    # Якщо аргументів немає — повертаємо default
+    if not args:
+        return default
+
+    # Якщо abs_mode=True — порівнюємо по абсолютному значенню
+    if abs_mode:
+        return max(args, key=lambda x: abs(x))
+    return max(args)
+
